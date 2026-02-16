cd00d8add9b608fb5c372fd9ae050d3d39941917
Vitaliy <113291299+dojomojo132@users.noreply.github.com>
2026-02-17 01:17:57 +0200

    BACKUP: 2026-02-17 перед деплой конфигурации

---
+++ _sync_prihod_module.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+"""Синхронизация BSL-модуля ПриходТовара между Конфигурация/ и Конфигурация/Проверка/"""
+import shutil
+import pathlib
+
+src = pathlib.Path(r"D:\Git\Public_Trade_Module\Конфигурация\Проверка\Documents\ПриходТовара\Forms\ФормаДокумента\Ext\Form\Module.bsl")
+dst = pathlib.Path(r"D:\Git\Public_Trade_Module\Конфигурация\Documents\ПриходТовара\Forms\ФормаДокумента\Ext\Form\Module.bsl")
+
+shutil.copy2(str(src), str(dst))
+print(f"OK: Скопировано {src.name} -> {dst.parent}")
